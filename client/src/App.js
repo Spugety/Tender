f1e5bf33caafcf4e5e6f1201832a6af06d60e8c1
Spugety <103894899+Spugety@users.noreply.github.com>
2022-11-29 14:56:51 -0500

    DO NOT MERGE--Test app

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 // importing components from react-router-dom package
 import {
 BrowserRouter as Router,
-Switch,
 Route,
-Redirect,
+Routes,
+// Redirect,
 } from "react-router-dom";
 
 // import Home component
@@ -19,12 +19,10 @@ return (
 	<>
 	{/* This is the alias of BrowserRouter i.e. Router */}
 	<Router>
-		<Switch>
-		{/* This route is for home component
+		<Routes>		{/* This route is for home component
 		with exact path "/", in component props
 		we passes the imported component*/}
 		<Route exact path="/" component={Home} />
-			
 		{/* This route is for about component
 		with exact path "/about", in component
 		props we passes the imported component*/}
@@ -38,8 +36,9 @@ return (
 		{/* If any route mismatches the upper
 		route endpoints then, redirect triggers
 		and redirects app to home component with to="/" */}
-		<Redirect to="/" />
-		</Switch>
+		{/* <Redirect to="/" /> */}
+    </Routes>
+
 	</Router>
 	</>
 );

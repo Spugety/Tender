f2bf1f488cb2d558377b45f0b995ddfcb8ace0f2
Spugety <103894899+Spugety@users.noreply.github.com>
2022-11-29 12:52:28 -0500

    DEPRECATED

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Home from './components/home'
-import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 
 // state is being used to render the new pages when clicked!
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,42 +1,10 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import logo from "../IMG/Tender Logo WIP.png";
 
-function MyForm() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(inputs);
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>username:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>password:
-        <input 
-          type="text" 
-          name="password" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <input type="submitButton" />
-    </form>
-  )
+function login()_ {
+    return(
+        <div className="login">
+            <header className="login"
+        </div>
+    )
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
